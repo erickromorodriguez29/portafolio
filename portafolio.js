e27628a4b45b98ead0bb138cd9398f41e680ac8b
Erick Romo <erick.romo.rodriguez29@gmail.com>
2025-10-19 16:18:52 -0600

    origin main

---
+++ portafolio.js
@@ -0,0 +1,10 @@
+
+// -- Efecto de navbar al hacer scroll --
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
